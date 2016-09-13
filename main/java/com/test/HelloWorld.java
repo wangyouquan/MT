00ef8b2da74904f91f2ccdba03f9412152e6a6d1
wangyouquan <304948851@qq.com>
2016-09-13 23:12:01 +0800

    11

--- main/java/com/test/HelloWorld.java
+++ main/java/com/test/HelloWorld.java
@@ -5,6 +5,6 @@ package com.test;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is my project and this commit!!");
+        System.out.println("This is my project and this commit!! hahaha");
     }
 }
