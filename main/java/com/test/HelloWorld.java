5bf827158a9683394b122df84c42165d87ff3dee
unknown <>
2016-09-13 21:52:48 +0800

    commit first time

---
+++ main/java/com/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by CC on 2016/9/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my project !");
+    }
+}
